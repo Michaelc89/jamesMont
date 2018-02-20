283a2e1f4737f71531a1a7e0db45fe09e1e6f059
Michael Christie <mikeychristie@gmail.com>
2018-02-20 19:33:28 +0000

    random string for ID working

--- jamesMont/jamesMont/Services/AzureService3.cs
+++ jamesMont/jamesMont/Services/AzureService3.cs
@@ -142,7 +142,27 @@ namespace jamesMont.Services
                     }
                 }
 
-                string x = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8);
+                // string x = Convert.ToBase64String(Guid.NewGuid().ToByteArray()).Substring(0, 8); 
+                var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+                var numbers = "0123456789";
+                var stringChars = new char[8];
+                var random = new Random();
+
+                for (int i = 0; i < 2; i++)
+                {
+                    stringChars[i] = chars[random.Next(chars.Length)];
+                }
+                for (int i = 2; i < 6; i++)
+                {
+                    stringChars[i] = numbers[random.Next(numbers.Length)];
+                }
+                for (int i = 6; i < 8; i++)
+                {
+                    stringChars[i] = chars[random.Next(chars.Length)];
+                }
+
+                string x = new String(stringChars);
+
                 var coffee = new OrderTBL()
                 {
                     id = x,
